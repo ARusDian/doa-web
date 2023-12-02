20af6a2621467175cd823d013acecea40de613f7
RUVNE <dian.galleon@gmail.com>
2023-12-02 15:56:26 +0800

    feat: index doa

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Service from './components/Service'
 import About from './components/About'
-
+import JuzAmma from './pages/juz-amma'
 
 function App() {
 
@@ -14,6 +14,7 @@ function App() {
       <Home/>
       <Service/>
       <About/>
+      {/* <JuzAmma/> */}
     </>
   )
 }
