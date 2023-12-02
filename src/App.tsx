811ba5c702a24235856a6599f7dae59500ceabce
ILHAMZKAI <89698221+ILHAMZKAI@users.noreply.github.com>
2023-12-02 21:56:11 +0800

    sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+<<<<<<< Updated upstream
+=======
+import Home from './pages/Home';
+import JuzAmma from './pages/juz-amma';
+import DoaList from './pages/doa/DoaList';
+import DetailSurah from './pages/juz-amma/detail';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+>>>>>>> Stashed changes
 
 import './App.css'
 import Navbar from './components/Navbar'
