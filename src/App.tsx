ade987f312e84d01c2c391f754bfe94e706c7fd0
ILHAMZKAI <89698221+ILHAMZKAI@users.noreply.github.com>
2023-12-02 16:10:51 +0800

    Revert "footer"
    This reverts commit 9aac90a1e02235aaf916ada3ce86c873f98f1da3.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Service from './components/Service'
 import About from './components/About'
-import Footer from './components/Footer'
 import JuzAmma from './pages/juz-amma'
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
       <Home/>
       <Service/>
       <About/>
-      <Footer/>
       {/* <JuzAmma/> */}
     </>
   )
