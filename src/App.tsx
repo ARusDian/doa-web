86c5e4657e322084a3f6ec599187ba3dcd707364
ILHAMZKAI <89698221+ILHAMZKAI@users.noreply.github.com>
2023-12-02 21:59:19 +0800

    Revert "sound"
    This reverts commit 811ba5c702a24235856a6599f7dae59500ceabce.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,3 @@
-<<<<<<< Updated upstream
-=======
-import Home from './pages/Home';
-import JuzAmma from './pages/juz-amma';
-import DoaList from './pages/doa/DoaList';
-import DetailSurah from './pages/juz-amma/detail';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
->>>>>>> Stashed changes
 
 import './App.css'
 import Navbar from './components/Navbar'
